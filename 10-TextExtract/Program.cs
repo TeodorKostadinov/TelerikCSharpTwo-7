a250079f6ebcb88afcac64092c6246ca04f83145
Teodor Kostadinov <teodor.kk.93@gmail.com>
2013-04-14 00:05:30 +0300

    1

---
+++ 10-TextExtract/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        using (StreamReader input = new StreamReader("../../input.txt"))
+        {
+            for (int i; (i = input.Read()) != -1; ) // Read char by char
+            {
+                if (i == '>') // Inside text node
+                {
+                    string text = String.Empty;
+
+                    while ((i = input.Read()) != -1 && i != '<') text += (char)i;
+
+                    if (!String.IsNullOrWhiteSpace(text)) Console.WriteLine(text.Trim());
+                }
+            }
+        }
+    }
+}
